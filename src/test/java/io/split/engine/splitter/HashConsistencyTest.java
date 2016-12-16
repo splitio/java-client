bd70ea622efb4048d63eeb988bdb63dfa0200dd8
Henry Jewkes <henry@relateiq.com>
2016-12-16 13:43:07 -0800

    Testing Guava for Comparison

--- src/test/java/io/split/engine/splitter/HashConsistencyTest.java
+++ src/test/java/io/split/engine/splitter/HashConsistencyTest.java
@@ -1,5 +1,6 @@
 package io.split.engine.splitter;
 
+import com.google.common.hash.Hashing;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class HashConsistencyTest {
     @Test
@@ -31,6 +33,18 @@ public class HashConsistencyTest {
     public void testMurmur3HashNonAlphaNum() throws IOException {
         File file = new File("src/test/resources", "murmur3-sample-data-non-alpha-numeric.csv");
         validateFileMurmur3Hash(file);
+    }
+
+    @Test
+    public void testGuavaMurmur3HashAlphaNum() throws IOException {
+        File file = new File("src/test/resources", "murmur3-sample-data.csv");
+        validateFileGuavaMurmur3Hash(file);
+    }
+
+    @Test
+    public void testGuavaMurmur3HashNonAlphaNum() throws IOException {
+        File file = new File("src/test/resources", "murmur3-sample-data-non-alpha-numeric.csv");
+        validateFileGuavaMurmur3Hash(file);
     }
 
     private void validateFileLegacyHash(File file) throws IOException {
@@ -67,6 +81,27 @@ public class HashConsistencyTest {
             int expected_bucket = Integer.parseInt(parts[3]);
 
             int hash = Splitter.hash(key, seed);
+            int bucket = Splitter.bucket(hash);
+
+            Assert.assertEquals(expected_hash, hash);
+            Assert.assertEquals(expected_bucket, bucket);
+        }
+    }
+
+    private void validateFileGuavaMurmur3Hash(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        reader.readLine(); // Header
+
+        String line;
+        int i = 0;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(",");
+            Integer seed = Integer.parseInt(parts[0]);
+            String key = parts[1];
+            int expected_hash = Integer.parseInt(parts[2]);
+            int expected_bucket = Integer.parseInt(parts[3]);
+
+            int hash = Hashing.murmur3_32(seed).hashString(key, Charset.forName("UTF-8")).asInt();
             int bucket = Splitter.bucket(hash);
 
             Assert.assertEquals(expected_hash, hash);
