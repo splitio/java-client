1fc2bc5e328c9559e910411247d9916f53d6f3ac
Henry Jewkes <hjewkes@gmail.com>
2017-02-08 11:12:05 -0800

    Test client (#12)
    * add description
    
    * 1.1
    
    * fixed labels
    
    * move more hashcodes to 7
    
    * revert to 1.1.1
    
    * fix typo in config
    
    * Rearrange project to accommodate testing
    
    * Pushing out tests
    
    * Removing Erroneous Result
    
    * Updating POM
    
    * Complete POM Updates
    
    * parent pom
    
    * add junit dep
    
    * hardcode the parent dependency
    
    * Removing Java 8 usages and Guava

---
+++ testing/src/main/java/io/split/client/testing/runner/RunWithSplits.java
@@ -0,0 +1,58 @@
+package io.split.client.testing.runner;
+
+import io.split.client.testing.SplitClientForTest;
+import io.split.client.testing.annotations.SplitTestClient;
+import org.junit.runners.model.Statement;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RunWithSplits extends Statement {
+    private final Object target;
+    private final Scenario scenario;
+    private final Statement next;
+
+    public RunWithSplits(Scenario scenario, Object target, Statement next) {
+        this.scenario = scenario;
+        this.target = target;
+        this.next = next;
+    }
+
+    @Override
+    public void evaluate() throws Throwable {
+        SplitClientForTest splitClient = findFirstSplitClient(target, target.getClass());
+
+        // Preserve the Split state between Test runs
+        Map<String, String> priorTests = new HashMap<>(splitClient.tests());
+
+        // Apply the Active Scenario for this
+        if (scenario != null) {
+            scenario.apply(splitClient);
+        }
+
+        next.evaluate();
+
+        // Clear any Scenario specific changes and re-apply existing splits
+        splitClient.clearTreatments();
+        splitClient.registerTreatments(priorTests);
+    }
+
+    private static SplitClientForTest findFirstSplitClient(Object target, Class<?> type) throws IllegalAccessException {
+        for (Field field : type.getDeclaredFields()) {
+            if (field.isAnnotationPresent(SplitTestClient.class)
+                    && field.getType().isAssignableFrom(SplitClientForTest.class)) {
+                field.setAccessible(true); // should work on private fields
+                if (field.get(target) instanceof SplitClientForTest) {
+                    return (SplitClientForTest) field.get(target);
+                }
+            }
+        }
+
+        if (type.getSuperclass() != null) {
+            return findFirstSplitClient(target, type.getSuperclass());
+        } else {
+            throw new IllegalArgumentException("No SplitTestClient found in hierarchy");
+        }
+    }
+}
