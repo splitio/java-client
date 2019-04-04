e24a0b60d69680a25fc371436a6593d8ee1762e0
Patricio Echague <patricioe@gmail.com>
2019-04-04 10:00:27 -0700

    remove another unexpected comment refactor

--- testing/src/test/java/io/split/client/testing/SplitTestAnnotationTest.java
+++ testing/src/test/java/io/split/client/testing/SplitTestAnnotationTest.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
 /**
  * SplitTestAnnotationTest
  * Demonstration & Test file for the use of SplitTests with the SplitTestRunner
- * SplitTestRunner is a JUnitTestRunner which executes annotated tests to achieve a variety of Split config
+ * SplitTestRunner is a JUnitTestRunner which executes annotated tests to achieve a variety of Split configurations
  */
 @RunWith(SplitTestRunner.class)
 public class SplitTestAnnotationTest {
