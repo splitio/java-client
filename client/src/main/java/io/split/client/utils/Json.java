bc408ddc761ed6f4dab517284566265fad0dac73
Martin Redolatti <martinredolatti@gmail.com>
2019-05-21 20:48:29 -0300

    allow Gson to serialize nulls

--- client/src/main/java/io/split/client/utils/Json.java
+++ client/src/main/java/io/split/client/utils/Json.java
@@ -1,13 +1,16 @@
 package io.split.client.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Created by adilaijaz on 6/10/16.
  */
 public class Json {
 
-    private static final Gson _json = new Gson();
+    private static final Gson _json = new GsonBuilder()
+            .serializeNulls()
+            .create();
 
     public static String toJson(Object obj) {
         return _json.toJson(obj);
