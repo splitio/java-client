7dbccc026dbb2d719322f815b48725f9a0bdd99b
Martin Redolatti <martinredolatti@gmail.com>
2019-05-21 21:40:55 -0300

    serialize integers as such. do not add decimal point

--- client/src/main/java/io/split/client/utils/Json.java
+++ client/src/main/java/io/split/client/utils/Json.java
@@ -2,6 +2,12 @@ package io.split.client.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
 
 /**
  * Created by adilaijaz on 6/10/16.
@@ -9,7 +15,17 @@ import com.google.gson.GsonBuilder;
 public class Json {
 
     private static final Gson _json = new GsonBuilder()
-            .serializeNulls()
+            .serializeNulls()  // Send nulls
+            .registerTypeAdapter(Double.class, new JsonSerializer<Double>() {
+                @Override
+
+                // Send integers as such
+                public JsonElement serialize(Double src, Type typeOfSrc, JsonSerializationContext context) {
+                    if (src == src.longValue())
+                        return new JsonPrimitive(src.longValue());
+                    return new JsonPrimitive(src);
+                }
+            })
             .create();
 
     public static String toJson(Object obj) {
