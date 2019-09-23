e0ee7e4628b11f8bde989e2b5c5384c9308c108e
Martin Redolatti <martinredolatti@gmail.com>
2019-09-23 11:46:12 -0300

    add tests for new integrations config

--- client/src/test/java/io/split/client/SplitClientConfigTest.java
+++ client/src/test/java/io/split/client/SplitClientConfigTest.java
@@ -1,7 +1,13 @@
 package io.split.client;
 
+import io.split.client.impressions.Impression;
 import io.split.client.impressions.ImpressionListener;
+import io.split.integrations.IntegrationsConfig;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class SplitClientConfigTest {
 
@@ -74,8 +80,39 @@ public class SplitClientConfigTest {
     @Test(expected = IllegalArgumentException.class)
     public void cannot_set_zero_capacity_on_impression_listener() throws InterruptedException {
         SplitClientConfig.builder()
-                .impressionListener(new ImpressionListener.NoopImpressionListener(), 0)
+                .integrations(IntegrationsConfig.builder()
+                        .impressionsListener(new ImpressionListener.NoopImpressionListener(), 0)
+                        .build())
+                .build();
+    }
+
+    @Test
+    public void config_does_not_crash_if_new_relic_class_not_present() {
+        SplitClientConfig cfg = SplitClientConfig.builder()
+                .integrations(IntegrationsConfig.builder()
+                        .newRelicImpressionListener()
+                        .build())
                 .build();
 
+        Assert.assertThat(
+                cfg.integrationsConfig().getImpressionsListeners(IntegrationsConfig.Execution.SYNC).size(),
+                is(equalTo(0)));
+    }
+
+    @Test
+    public void old_impression_listener_config_still_works() {
+        SplitClientConfig cfg = SplitClientConfig.builder()
+               .impressionListener(new ImpressionListener() {
+                   @Override
+                   public void log(Impression impression) { /* noop */ }
+
+                   @Override
+                   public void close() { /* noop */ }
+               }, 1000)
+                .build();
+
+        Assert.assertThat(
+                cfg.integrationsConfig().getImpressionsListeners(IntegrationsConfig.Execution.ASYNC).size(),
+                is(equalTo(1)));
     }
 }
