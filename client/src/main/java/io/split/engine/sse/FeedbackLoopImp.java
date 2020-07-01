fbf0d59ba8c2c9e2c04613b1b7fd1464c8a3bb60
Mauro Sanz <mauro.sanz@split.io>
2020-07-01 14:55:59 -0300

    wip

--- client/src/main/java/io/split/engine/sse/FeedbackLoopImp.java
+++ client/src/main/java/io/split/engine/sse/FeedbackLoopImp.java
@@ -5,11 +5,6 @@ import io.split.engine.sse.dtos.IncomingNotification;
 
 public class FeedbackLoopImp implements FeedbackLoopListener {
     @Override
-    public void onMessageNotificationAdded(IncomingNotification incomingNotification) {
-        // TODO: implement this.
-    }
-
-    @Override
     public void onErrorNotificationAdded(ErrorNotification errorNotification) {
         // TODO: implement this.
     }
--- client/src/main/java/io/split/engine/sse/FeedbackLoopListener.java
+++ client/src/main/java/io/split/engine/sse/FeedbackLoopListener.java
@@ -1,10 +1,8 @@
 package io.split.engine.sse;
 
 import io.split.engine.sse.dtos.ErrorNotification;
-import io.split.engine.sse.dtos.IncomingNotification;
 
 public interface FeedbackLoopListener {
-    void onMessageNotificationAdded(IncomingNotification incomingNotification);
     void onErrorNotificationAdded(ErrorNotification errorNotification);
     void onConnected();
     void onDisconnect();
