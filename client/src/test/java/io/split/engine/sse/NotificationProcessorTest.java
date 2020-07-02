f57081790dc1378ac55682fded9893bcfe15eb91
Mauro Sanz <mauro.sanz@split.io>
2020-07-02 16:43:20 -0300

    add test

--- client/src/test/java/io/split/engine/sse/NotificationProcessorTest.java
+++ client/src/test/java/io/split/engine/sse/NotificationProcessorTest.java
@@ -1,41 +1,61 @@
 package io.split.engine.sse;
 
 import io.split.engine.sse.dtos.*;
+import io.split.engine.sse.workers.SegmentsWorkerImp;
 import io.split.engine.sse.workers.SplitsWorker;
+import io.split.engine.sse.workers.Worker;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class NotificationProcessorTest {
+    private final SplitsWorker _splitsWorker;
+    private final Worker<SegmentQueueDto> _segmentWorker;
+    private final NotificationProcessor _notificationProcessor;
+
+    public NotificationProcessorTest() {
+        _splitsWorker = Mockito.mock(SplitsWorker.class);
+        _segmentWorker = Mockito.mock(SegmentsWorkerImp.class);
+
+        _notificationProcessor = new NotificationProcessorImp(_splitsWorker, _segmentWorker);
+    }
 
     @Test
     public void processSplitUpdateAddToQueueInWorker() {
         long changeNumber = 1585867723838L;
         String channel = "splits";
         GenericNotificationData genericNotificationData = new GenericNotificationData(changeNumber, null, null, null, null, null, null, channel);
-
-        SplitsWorker splitsWorker = Mockito.mock(SplitsWorker.class);
         SplitChangeNotification splitChangeNotification = new SplitChangeNotification(genericNotificationData);
 
-        NotificationProcessor notificationProcessor = new NotificationProcessorImp(splitsWorker);
-        notificationProcessor.process(splitChangeNotification);
+        _notificationProcessor.process(splitChangeNotification);
 
-        Mockito.verify(splitsWorker, Mockito.times(1)).addToQueue(splitChangeNotification.getChangeNumber());
+        Mockito.verify(_splitsWorker, Mockito.times(1)).addToQueue(splitChangeNotification.getChangeNumber());
     }
 
     @Test
-    public void processSplitKillAddToQueueInWorker() {
+    public void processSplitKillAndAddToQueueInWorker() {
         long changeNumber = 1585867723838L;
         String defaultTreatment = "off";
         String splitName = "test-split";
         String channel = "splits";
         GenericNotificationData genericNotificationData = new GenericNotificationData(changeNumber, defaultTreatment, splitName, null, null, null, null, channel);
-
-        SplitsWorker splitsWorker = Mockito.mock(SplitsWorker.class);
         SplitKillNotification splitKillNotification = new SplitKillNotification(genericNotificationData);
 
-        NotificationProcessor notificationProcessor = new NotificationProcessorImp(splitsWorker);
-        notificationProcessor.process(splitKillNotification);
+        _notificationProcessor.process(splitKillNotification);
 
-        Mockito.verify(splitsWorker, Mockito.times(1)).killSplit(splitKillNotification.getChangeNumber(), splitKillNotification.getSplitName(), splitKillNotification.getDefaultTreatment());
+        Mockito.verify(_splitsWorker, Mockito.times(1)).killSplit(splitKillNotification.getChangeNumber(), splitKillNotification.getSplitName(), splitKillNotification.getDefaultTreatment());
+        Mockito.verify(_splitsWorker, Mockito.times(1)).addToQueue(splitKillNotification.getChangeNumber());
+    }
+
+    @Test
+    public void processSegmentUpdateAddToQueueInWorker() {
+        long changeNumber = 1585867723838L;
+        String segmentName = "segment-test";
+        String channel = "segments";
+        GenericNotificationData genericNotificationData = new GenericNotificationData(changeNumber, null, null, null, null, segmentName, null, channel);
+        SegmentChangeNotification segmentChangeNotification = new SegmentChangeNotification(genericNotificationData);
+
+        _notificationProcessor.process(segmentChangeNotification);
+
+        Mockito.verify(_segmentWorker, Mockito.times(1)).addToQueue(Mockito.any(SegmentQueueDto.class));
     }
 }
