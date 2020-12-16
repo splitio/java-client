e30b7b0078736239901bdfd0214e961dd16cdc1d
Mauro Sanz <mauro.sanz@split.io>
2020-12-16 18:56:05 -0300

    pr feedback

--- client/src/main/java/io/split/client/SplitManagerImpl.java
+++ client/src/main/java/io/split/client/SplitManagerImpl.java
@@ -10,7 +10,13 @@ import io.split.engine.experiments.ParsedSplit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import java.util.concurrent.TimeoutException;
 
 /**
