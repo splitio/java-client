196e7892d6e673bcb5e7a8b740e66d26ce47975b
Lucas Echeverz <lucas.echeverz@split.io>
2021-04-26 13:03:44 -0300

    Adding config Generator

--- client/src/main/java/io/split/telemetry/synchronizer/TelemetrySynchronizerImp.java
+++ client/src/main/java/io/split/telemetry/synchronizer/TelemetrySynchronizerImp.java
@@ -3,6 +3,10 @@ package io.split.telemetry.synchronizer;
 import io.split.cache.SegmentCache;
 import io.split.cache.SplitCache;
 import io.split.client.SplitClientConfig;
+import io.split.client.impressions.ImpressionListener;
+import io.split.client.impressions.ImpressionsManager;
+import io.split.integrations.IntegrationsConfig;
+import io.split.integrations.NewRelicListener;
 import io.split.telemetry.domain.Config;
 import io.split.telemetry.domain.Rates;
 import io.split.telemetry.domain.Stats;
@@ -14,10 +18,15 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TelemetrySynchronizerImp implements TelemetrySynchronizer{
+
+    private static final int OPERATION_MODE = 0;
+    private static  final String STORAGE = "memory";
 
     private HttpTelemetryMemorySender _httpHttpTelemetryMemorySender;
     private TelemetryStorageConsumer _teleTelemetryStorageConsumer;
@@ -69,6 +78,9 @@ public class TelemetrySynchronizerImp implements TelemetrySynchronizer{
         Config config = new Config();
         Rates rates = new Rates();
         URLOverrides urlOverrides = new URLOverrides();
+        List<IntegrationsConfig.ImpressionListenerWithMeta> impressionsListeners = splitClientConfig.integrationsConfig().getImpressionsListeners(IntegrationsConfig.Execution.ASYNC);
+        impressionsListeners.addAll(splitClientConfig.integrationsConfig().getImpressionsListeners(IntegrationsConfig.Execution.SYNC));
+        List<String> impressions = getImpressions(impressionsListeners);
 
         rates.set_telemetry(splitClientConfig.get_telemetryRefreshRate());
         rates.set_events(splitClientConfig.eventFlushIntervalInMillis());
@@ -84,16 +96,16 @@ public class TelemetrySynchronizerImp implements TelemetrySynchronizer{
 
         config.set_burTimeouts(_teleTelemetryStorageConsumer.getBURTimeouts());
         config.set_nonReadyUsages(_teleTelemetryStorageConsumer.getNonReadyUsages());
-        config.set_impressionsListenerEnabled(splitClientConfig.);
         config.set_httpProxyDetected(splitClientConfig.proxy() != null);
-        config.set_impressionsMode(splitClientConfig.impressionsMode());
-        config.set_integrations(splitClientConfig);
-        config.set_operationMode(0); //Standalone -> 0
-        config.set_storage("memory");
+        config.set_impressionsMode(getImpressionsMode(splitClientConfig));
+        config.set_integrations(impressions);
+        config.set_impressionsListenerEnabled((impressionsListeners.size()-impressions.size()) > 0);
+        config.set_operationMode(OPERATION_MODE);
+        config.set_storage(STORAGE);
         config.set_impressionsQueueSize(splitClientConfig.impressionsQueueSize());
         config.set_redundantFactories(getRedundantFactories(factoryInstances));
         config.set_eventsQueueSize(splitClientConfig.eventsQueueSize());
-        config.set_tags(tags);
+        config.set_tags(getListMaxSize(tags));
         config.set_activeFactories(factoryInstances.size());
         config.set_timeUntilReady(timeUntilReady);
         config.set_rates(rates);
@@ -108,5 +120,24 @@ public class TelemetrySynchronizerImp implements TelemetrySynchronizer{
             count = count + l - 1l;
         }
         return count;
+    }
+
+    private int getImpressionsMode(SplitClientConfig config) {
+        return ImpressionsManager.Mode.OPTIMIZED.equals(config.impressionsMode()) ? 0 : 1;
+    }
+
+    private List<String> getListMaxSize(List<String> list) {
+        return list.size()> 10 ? list.subList(0, 10) : list;
+    }
+
+    private List<String> getImpressions(List<IntegrationsConfig.ImpressionListenerWithMeta> impressionsListeners) {
+        List<String> impressions = new ArrayList<>();
+        for(IntegrationsConfig.ImpressionListenerWithMeta il: impressionsListeners) {
+            ImpressionListener listener = il.listener();
+            if(listener instanceof NewRelicListener) {
+                impressions.add(NewRelicListener.class.getName());
+            }
+        }
+        return impressions;
     }
 }
---
+++ client/src/test/java/io/split/telemetry/synchronizer/TelemetrySynchronizerImpTest.java
@@ -0,0 +1,13 @@
+package io.split.telemetry.synchronizer;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TelemetrySynchronizerImpTest{
+    
+
+}
