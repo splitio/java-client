2b523e92bfeff02ea0f274197db36d5bc3ebad7b
Nadia Mayor <nadia.mayor@split.io>
2022-08-10 17:30:40 -0300

    Add test case for HasPipeline

--- client/src/test/java/io/split/client/impressions/RedisImpressionSenderTest.java
+++ client/src/test/java/io/split/client/impressions/RedisImpressionSenderTest.java
@@ -1,10 +1,14 @@
 package io.split.client.impressions;
 
+import io.split.storages.pluggable.CustomStorageWrapperHasPipeline;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 import pluggable.CustomStorageWrapper;
 
 import java.util.HashMap;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentMap;
 
 public class RedisImpressionSenderTest {
 
@@ -15,8 +19,24 @@ public class RedisImpressionSenderTest {
 
         HashMap<ImpressionCounter.Key, Integer> counters =  new HashMap<>();
         ImpressionCounter.Key counterKey1 =  new ImpressionCounter.Key("feature1", 100);
-        counters.put(counterKey1,2);
+        counters.put(counterKey1, 2);
         redisImpressionSender.postCounters(counters);
         Mockito.verify(customStorageWrapper, Mockito.times(1)).hIncrement(Mockito.eq("SPLITIO.impressions.count"), Mockito.eq("feature1::100"), Mockito.eq(2L));
+    }
+
+    @Test
+    public void testPostCountersHasPipeline() throws Exception {
+        CustomStorageWrapperHasPipeline customStorageWrapper = new CustomStorageWrapperHasPipeline();
+        RedisImpressionSender redisImpressionSender = RedisImpressionSender.create(customStorageWrapper);
+
+        HashMap<ImpressionCounter.Key, Integer> counters =  new HashMap<>();
+        ImpressionCounter.Key counterKey1 =  new ImpressionCounter.Key("feature1", 100);
+        counters.put(counterKey1, 2);
+        redisImpressionSender.postCounters(counters);
+
+        ConcurrentMap<String, Long> impressionsCount = customStorageWrapper.getImpressionsCount();
+        Assert.assertTrue(impressionsCount.containsKey("feature1::100"));
+        String key = "feature1::100";
+        Assert.assertEquals(Optional.of(2L), Optional.of(impressionsCount.get(key)));
     }
 }
