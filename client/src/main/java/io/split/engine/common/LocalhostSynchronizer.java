721a0f1c45cf283397c71d08d0a6a1b5d794518d
Nadia Mayor <nadia.mayor@split.io>
2022-11-28 12:55:05 -0300

    Update LocalhostSynchronizer

--- client/src/main/java/io/split/engine/common/LocalhostSynchronizer.java
+++ client/src/main/java/io/split/engine/common/LocalhostSynchronizer.java
@@ -1,45 +1,46 @@
 package io.split.engine.common;
 
-public enum LocalhostSynchronizer implements Synchronizer{
+public enum LocalhostSynchronizer implements Synchronizer {
     ;
 
     @Override
     public boolean syncAll() {
+        //Todo implement
         return false;
     }
 
     @Override
     public void startPeriodicFetching() {
-
+        //todo implement
     }
 
     @Override
     public void stopPeriodicFetching() {
-
+        // todo implement
     }
 
     @Override
     public void refreshSplits(long targetChangeNumber) {
-
+        // todo implement
     }
 
     @Override
     public void localKillSplit(String splitName, String defaultTreatment, long newChangeNumber) {
-
+        //No-Op
     }
 
     @Override
     public void refreshSegment(String segmentName, long targetChangeNumber) {
-
+        // todo implement
     }
 
     @Override
     public void startPeriodicDataRecording() {
-
+        //No-Op
     }
 
     @Override
     public void stopPeriodicDataRecording(long splitCount, long segmentCount, long segmentKeyCount) {
-
+        //No-Op
     }
 }
