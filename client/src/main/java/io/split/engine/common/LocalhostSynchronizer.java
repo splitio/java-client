b08bdc0b084fa1ae89cfe6635a37e0bdf026a105
Nadia Mayor <nadia.mayor@split.io>
2022-12-02 17:00:31 -0300

    Add space

--- client/src/main/java/io/split/engine/common/LocalhostSynchronizer.java
+++ client/src/main/java/io/split/engine/common/LocalhostSynchronizer.java
@@ -11,7 +11,7 @@ public class LocalhostSynchronizer extends SynchronizerImp{
                                  SplitFetcher splitFetcher,
                                  SplitCacheProducer splitCacheProducer,
                                  SegmentCacheProducer segmentCacheProducer){
-        super(splitTasks, splitFetcher, splitCacheProducer, segmentCacheProducer,0,0,0,false);
+        super(splitTasks, splitFetcher, splitCacheProducer, segmentCacheProducer, 0, 0, 0, false);
     }
 
     @Override
