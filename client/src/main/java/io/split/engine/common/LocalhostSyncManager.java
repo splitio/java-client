867921958b6ec819b886f0de894a530307e8c35e
Nadia Mayor <nadia.mayor@split.io>
2022-12-02 16:56:47 -0300

    [SDKS-6269] Create LocalhostSyncManager

---
+++ client/src/main/java/io/split/engine/common/LocalhostSyncManager.java
@@ -0,0 +1,16 @@
+package io.split.engine.common;
+
+import java.io.IOException;
+
+public class LocalhostSyncManager implements SyncManager {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown(long splitCount, long segmentCount, long segmentKeyCount) throws IOException {
+
+    }
+}
