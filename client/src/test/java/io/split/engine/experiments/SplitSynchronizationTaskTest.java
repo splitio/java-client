7d5ca8b89efe102533b25f93ccaaa51255e10fc7
Nadia Mayor <nadia.mayor@split.io>
2022-12-02 11:38:55 -0300

    [SDKS-6272] Add test cases

--- client/src/test/java/io/split/engine/experiments/SplitSynchronizationTaskTest.java
+++ client/src/test/java/io/split/engine/experiments/SplitSynchronizationTaskTest.java
@@ -6,7 +6,6 @@ import io.split.storages.SplitCacheProducer;
 import io.split.storages.memory.InMemoryCacheImp;
 import io.split.telemetry.storage.NoopTelemetryStorage;
 import io.split.telemetry.storage.TelemetryStorage;
-import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -15,17 +14,34 @@ public class SplitSynchronizationTaskTest {
     private static final TelemetryStorage TELEMETRY_STORAGE_NOOP = Mockito.mock(NoopTelemetryStorage.class);
 
     @Test
-    public void testLocalhost() {
+    public void testLocalhost() throws InterruptedException {
         SplitCacheProducer splitCacheProducer = new InMemoryCacheImp();
         SplitCacheConsumer splitCacheConsumer = Mockito.mock(SplitCacheConsumer.class);
 
-        SplitChangeFetcher splitChangeFetcher = new LocalhostSplitChangeFetcher("src/test/resources/split_init.json");
+        //SplitChangeFetcher splitChangeFetcher = new LocalhostSplitChangeFetcher("src/test/resources/split_init.json");
+        SplitChangeFetcher splitChangeFetcher = Mockito.mock(LocalhostSplitChangeFetcher.class);
         SplitParser splitParser = new SplitParser();
-        FetchOptions fetchOptions = Mockito.mock(FetchOptions.class);
+        FetchOptions fetchOptions = new FetchOptions.Builder().build();
         SplitFetcher splitFetcher = new SplitFetcherImp(splitChangeFetcher, splitParser, splitCacheConsumer, splitCacheProducer, TELEMETRY_STORAGE_NOOP);
 
-        FetchResult fetchResult = splitFetcher.forceRefresh(fetchOptions);
+        SplitSynchronizationTask splitSynchronizationTask = new SplitSynchronizationTask(splitFetcher, splitCacheProducer, 1000);
 
-        Assert.assertEquals(1, fetchResult.getSegments().size());
+        splitSynchronizationTask.start();
+
+        Thread.sleep(2000);
+
+        Mockito.verify(splitChangeFetcher, Mockito.times(1)).fetch(-1, fetchOptions);
+    }
+
+    @Test
+    public void testStartAndStop() throws InterruptedException {
+        SplitCacheProducer splitCacheProducer = new InMemoryCacheImp();
+        SplitFetcherImp splitFetcherImp = Mockito.mock(SplitFetcherImp.class);
+        SplitSynchronizationTask splitSynchronizationTask = new SplitSynchronizationTask(splitFetcherImp, splitCacheProducer, 1000);
+        splitSynchronizationTask.start();
+
+        Thread.sleep(2000);
+
+        Mockito.verify(splitFetcherImp, Mockito.times(1)).run();
     }
 }
