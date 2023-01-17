a27a1cf8319d49d01a4aacb96f750fd48e9dbbc5
Nadia Mayor <nadia.mayor@split.io>
2023-01-17 17:27:46 -0300

    Update log

--- client/src/main/java/io/split/service/HttpPostImp.java
+++ client/src/main/java/io/split/service/HttpPostImp.java
@@ -40,7 +40,7 @@ public class HttpPostImp {
             int status = response.getCode();
             if (status < HttpStatus.SC_OK || status >= HttpStatus.SC_MULTIPLE_CHOICES) {
                 _telemetryRuntimeProducer.recordSyncError(httpParamsWrapper.getResourceEnum(), status);
-                _logger.warn(String.format("Response status was: %s. Reason: %s", status , response.getEntity().toString()));
+                _logger.warn(String.format("Response status was: %s. Reason: %s", status , response.getReasonPhrase()));
                 return;
             }
             _telemetryRuntimeProducer.recordSyncLatency(httpParamsWrapper.getHttpLatenciesEnum(), System.currentTimeMillis() - initTime);
