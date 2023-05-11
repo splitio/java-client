2ac301f4ab6f19dd81e88ed49989533cf2b25ecc
Nadia Mayor <nadia.mayor@split.io>
2023-05-11 11:31:16 -0300

    [SDKS-6960] Update FilterAdapter

--- client/src/main/java/io/split/client/impressions/filters/FilterAdapter.java
+++ client/src/main/java/io/split/client/impressions/filters/FilterAdapter.java
@@ -2,7 +2,7 @@ package io.split.client.impressions.filters;
 
 public interface FilterAdapter {
 
-    boolean add(String featureName, String key);
-    boolean contains(String featureName, String key);
+    boolean add(String featureFlagName, String key);
+    boolean contains(String featureFlagName, String key);
     void clear();
 }
