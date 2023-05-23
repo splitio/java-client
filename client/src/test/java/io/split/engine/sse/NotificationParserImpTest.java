07baebcd83163300033df6c35a884c04a87f1095
Nadia Mayor <nadia.mayor@split.io>
2023-05-23 14:19:12 -0300

    [SDKS-7028] Remove imports

--- client/src/test/java/io/split/engine/sse/NotificationParserImpTest.java
+++ client/src/test/java/io/split/engine/sse/NotificationParserImpTest.java
@@ -6,8 +6,6 @@ import io.split.engine.sse.exceptions.EventParsingException;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class NotificationParserImpTest {
 
