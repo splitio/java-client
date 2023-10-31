b9fef3a3a9379bd7c6c29e878b51bd82b7737fc0
Nadia Mayor <nadia.mayor@split.io>
2023-10-31 11:10:55 -0300

    Remove imports not using

--- client/src/main/java/io/split/client/SplitClient.java
+++ client/src/main/java/io/split/client/SplitClient.java
@@ -3,7 +3,6 @@ package io.split.client;
 import io.split.client.api.Key;
 import io.split.client.api.SplitResult;
 
-import java.awt.*;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
