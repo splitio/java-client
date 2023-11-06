e43301dee18e9b5d4cd6c9821334c14e163a38ed
Mauro Antonio Sanz <mauro.sanz@split.io>
2023-11-06 13:53:16 -0300

    fixed test

--- client/src/test/java/io/split/engine/common/FetcherOptionsTest.java
+++ client/src/test/java/io/split/engine/common/FetcherOptionsTest.java
@@ -1,9 +1,7 @@
 package io.split.engine.common;
 
-import org.apache.hc.core5.http.Header;
 import org.junit.Test;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -13,15 +11,6 @@ public class FetcherOptionsTest {
 
     @Test
     public void optionsPropagatedOk() {
-        final boolean[] called = {false};
-        Function<Map<String, String>, Void> func = new Function<Map<String, String>, Void>() {
-            @Override
-            public  Void apply(Map<String, String> unused) {
-                called[0] = true;
-                return null;
-            }
-        };
-
         FetchOptions options = new FetchOptions.Builder()
                 .cacheControlHeaders(true)
                 .fastlyDebugHeader(true)
@@ -32,7 +21,6 @@ public class FetcherOptionsTest {
         assertEquals(options.cacheControlHeadersEnabled(), true);
         assertEquals(options.fastlyDebugHeaderEnabled(), true);
         assertEquals(options.targetCN(), 123);
-        assertEquals(called[0], true);
         assertEquals("set1,set2", options.flagSetsFilter());
     }
 }
