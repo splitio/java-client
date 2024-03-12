9d58e0079a6acb664417c013689499a5c2821ff6
Bilal Al <bilal.al-shahwany@split.io>
2024-03-11 20:27:56 -0700

    added request decorator class

---
+++ client/src/main/java/io/split/client/RequestDecorator.java
@@ -0,0 +1,63 @@
+package io.split.client;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
+
+import java.util.*;
+
+interface UserCustomHeaderDecorator
+{
+    Map<String, String> getHeaderOverrides();
+}
+
+class NoOpHeaderDecorator implements  UserCustomHeaderDecorator {
+    public NoOpHeaderDecorator() {}
+    @Override
+    public Map<String, String> getHeaderOverrides() {
+        return new HashMap<String, String>();
+    }
+}
+
+class RequestDecorator {
+    UserCustomHeaderDecorator _headerDecorator;
+
+    private static final Set<String> forbiddenHeaders = new HashSet<>(Arrays.asList(
+            "SplitSDKVersion",
+            "SplitMachineIp",
+            "SplitMachineName",
+            "SplitImpressionsMode",
+            "Host",
+            "Referrer",
+            "Content-Type",
+            "Content-Length",
+            "Content-Encoding",
+            "Accept",
+            "Keep-Alive",
+            "X-Fastly-Debug"
+    ));
+
+    public RequestDecorator(UserCustomHeaderDecorator headerDecorator) {
+        _headerDecorator = (headerDecorator == null)
+                ? new NoOpHeaderDecorator()
+                : headerDecorator;
+    }
+
+    public HttpUriRequestBase decorateHeaders(HttpUriRequestBase request) {
+        try {
+            Map<String, String> headers = _headerDecorator.getHeaderOverrides();
+            for (Map.Entry entry : headers.entrySet()) {
+                if (isHeaderAllowed(entry.getKey().toString())) {
+                    request.addHeader(entry.getKey().toString(), entry.getValue());
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException(String.format("Problem adding custom headers to request decorator: %s", e), e);
+        }
+
+        return request;
+    }
+
+    private boolean isHeaderAllowed(String headerName) {
+        return !forbiddenHeaders.contains(headerName);
+    }
+}
