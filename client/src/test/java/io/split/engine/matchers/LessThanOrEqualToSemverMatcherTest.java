28ef21004ded9798c5c535242462bc64b8e6c1a0
Bilal Al <bilal.al-shahwany@split.io>
2024-05-02 16:01:48 -0700

    polish

--- client/src/test/java/io/split/engine/matchers/LessThanOrEqualToSemverMatcherTest.java
+++ client/src/test/java/io/split/engine/matchers/LessThanOrEqualToSemverMatcherTest.java
@@ -3,6 +3,7 @@ package io.split.engine.matchers;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Tests for EqualToSemverMatcher
@@ -14,10 +15,10 @@ public class LessThanOrEqualToSemverMatcherTest {
     public void works() {
         LessThanOrEqualToSemverMatcher lessThanOrEqualToSemverMatcher = new LessThanOrEqualToSemverMatcher("2.1.8");
 
-        assertTrue( lessThanOrEqualToSemverMatcher.match("2.1.8", null, null, null) == true);
-        assertTrue( lessThanOrEqualToSemverMatcher.match("2.1.9", null, null, null) == false);
-        assertTrue( lessThanOrEqualToSemverMatcher.match("2.1.8-rc", null, null, null) == true);
-        assertTrue( lessThanOrEqualToSemverMatcher.match("2.0.10", null, null, null) == true);
-        assertTrue( lessThanOrEqualToSemverMatcher.match("2.1.8+build", null, null, null) == true);
+        assertTrue( lessThanOrEqualToSemverMatcher.match("2.1.8", null, null, null));
+        assertFalse( lessThanOrEqualToSemverMatcher.match("2.1.9", null, null, null));
+        assertTrue( lessThanOrEqualToSemverMatcher.match("2.1.8-rc", null, null, null));
+        assertTrue( lessThanOrEqualToSemverMatcher.match("2.0.10", null, null, null));
+        assertTrue( lessThanOrEqualToSemverMatcher.match("2.1.8+build", null, null, null));
     }
 }
