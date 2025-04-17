6f82e8958a31671cf40d954c67c00b8211283ad7
Bilal Al-Shahwany <bilal.al-shahwany@harness.io>
2025-04-16 21:29:01 -0700

    fix build

--- client/src/main/java/io/split/storages/RuleBasedSegmentCacheProducer.java
+++ client/src/main/java/io/split/storages/RuleBasedSegmentCacheProducer.java
@@ -7,5 +7,6 @@ import java.util.List;
 public interface RuleBasedSegmentCacheProducer {
     boolean remove(String name);
     void setChangeNumber(long changeNumber);
+    long getChangeNumber();
     void update(List<ParsedRuleBasedSegment> toAdd, List<String> toRemove, long changeNumber);
 }
