0b14464c2fe6c9f237007166be68f7febb6473df
Bilal Al-Shahwany <41021307+chillaq@users.noreply.github.com>
2025-04-25 10:29:13 -0700

    Update client/src/main/java/io/split/client/dtos/SplitChangesOldPayloadDto.java
    Co-authored-by: Mauro Sanz <51236193+sanzmauro@users.noreply.github.com>

--- client/src/main/java/io/split/client/dtos/SplitChangesOldPayloadDto.java
+++ client/src/main/java/io/split/client/dtos/SplitChangesOldPayloadDto.java
@@ -14,12 +14,20 @@ public class SplitChangesOldPayloadDto {
     @SerializedName("splits")
     public List<Split> d;
 
-    public ChangeDto<Split> toChangeDTO() {
-        ChangeDto<Split> dto = new ChangeDto<>();
-        dto.s = this.s;
-        dto.t = this.t;
-        dto.d = this.d;
-        return dto;
-
+    public SplitChange toSplitChange() {
+        SplitChange splitChange = new SplitChange();
+        ChangeDto<Split> ff = new ChangeDto<>();
+        ff.s = this.s;
+        ff.t = this.t;
+        ff.d = this.d;
+        ChangeDto<RuleBasedSegment> rbs = new ChangeDto<>();
+        rbs.d = new ArrayList<>();
+        rbs.t = -1;
+        rbs.s = -1;
+        
+        splitChange.ff = ff;
+        splitChange.rbs = rbs;
+        
+        return splitChange;
     }
 }
