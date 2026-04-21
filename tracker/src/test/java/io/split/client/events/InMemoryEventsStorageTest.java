c65dbb9682305a9871ee29feb4bd95e9c7018fec
Gaston Thea <gaston.thea@harness.io>
2026-04-21 15:06:00 -0300

    Semver fix

--- tracker/src/test/java/io/split/client/events/InMemoryEventsStorageTest.java
+++ tracker/src/test/java/io/split/client/events/InMemoryEventsStorageTest.java
@@ -31,7 +31,7 @@ public class InMemoryEventsStorageTest {
     @Test
     public void trackNullEventReturnsFalse() {
         assertFalse(_storage.track(null, 0));
-        verifyZeroInteractions(_stats);
+        verifyNoInteractions(_stats);
     }
 
     @Test
